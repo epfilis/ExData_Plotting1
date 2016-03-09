b302ff6341bf62de40c540f048d2df2d1ea098d9
epfilis <epfilis@users.noreply.github.com>
2016-03-08 21:21:23 -0600

    Added files via upload

--- plot3.R
+++ plot3.R
@@ -1,5 +1,5 @@
 ## Big dataset
-bigdata <- read.table("household_power_consumption.txt", header=T, sep=";")
+bigdata <- read.table("household_power_consumption.txt", header=T, sep=";", na.strings = "?")
 data<-bigdata[bigdata$Date %in% c("1/2/2007","2/2/2007") ,]
 
 ## Converting dates
@@ -13,7 +13,9 @@ subMetering3 <- as.numeric(data$Sub_metering_3)
 plot(datetime, subMetering1, type="l", ylab="Energy Submetering", xlab="")
 lines(datetime, subMetering2, type="l", col="red")
 lines(datetime, subMetering3, type="l", col="blue")
-legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=2, lwd=4.5, col=c("black", "red", "blue"))
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=1, col=c("black", "red", "blue"))
 dev.copy(png, file="plot3.png", width=480, height=480)
 dev.off()
+
+
 
--- plot4.R
+++ plot4.R
@@ -1,5 +1,5 @@
 ## Big dataset
-bigdata <- read.table("household_power_consumption.txt", header=T, sep=";")
+bigdata <- read.table("household_power_consumption.txt", header=T, sep=";" , na.strings = "?")
 data<-bigdata[bigdata$Date %in% c("1/2/2007","2/2/2007") ,]
 
 ## Converting dates
@@ -21,10 +21,12 @@ plot(datetime, voltage, type="l", xlab="datetime", ylab="Voltage")
 plot(datetime, subMetering1, type="l", ylab="Energy Submetering", xlab="")
 lines(datetime, subMetering2, type="l", col="red")
 lines(datetime, subMetering3, type="l", col="blue")
-legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=, lwd=2.5, col=c("black", "red", "blue"), bty="o")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty= 1, lwd=1, col=c("black", "red", "blue"), bty="o")
 
 plot(datetime, globalReactivePower, type="l", xlab="datetime", ylab="Global_reactive_power")
 dev.copy(png, file="plot4.png", width=480, height=480)
 dev.off()
+
+
 
 
